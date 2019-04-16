1be817ee20517b6d1dc6af546b607509201418db
coffee-junjun <coffeesugarsi5@gmail.com>
2019-04-16 10:13:22 +0900

    new file

---
+++ javarun.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FILE=${1}
+JAVA=${FILE##*/}
+JAVAFILE=${JAVA%.*}
+
+if [ `echo $1 | grep /` ]; then # 絶対パス､相対パスの処理
+	DIR=$PWD #現在のディレクトリの格納
+	CD=${FILE%/*} #移動先のディレクトリ
+	cd ${CD}/ && javac -encoding Shift_JIS ${JAVA} && java ${JAVAFILE} && rm ${JAVAFILE}.class && cd ${DIR}
+else
+	javac -encoding Shift_JIS ${JAVA} && java ${JAVAFILE} && rm ${JAVAFILE}.class
+fi
